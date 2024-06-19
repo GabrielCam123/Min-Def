bb9fb25a535767bdac51c26369c725d167f2de53
Oscar Gabriel <cameotapia@gmail.com>
2024-06-19 19:08:12 -0400

    Ultimo19

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from "./header"
 import "./Formulario.css"
+import Edan from "./edan"
 function Formulario() {
   return (
     <div classname='estiloFormulario'>
         <Header/>
+        <div>
+          <Edan/>          
+        </div>
+
     </div>
   )
 }
---
+++ src/components/edan.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import "./edan.css"
+
+function edan() {
+  return (
+    <div className='contenedorEdan'>
+      <p>EVALUACION DE DAÑOS Y  ANALISIS DE NECESIDADES</p>
+    </div>
+  )
+}
+
+export default edan
