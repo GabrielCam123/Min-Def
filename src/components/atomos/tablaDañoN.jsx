a33e97f3eee4e7e79746ee1185ee2100ea077e04
Oscar Gabriel <cameotapia@gmail.com>
2024-06-26 16:07:18 -0400

    ultimo2606

---
+++ src/components/atomos/tablaDañoN.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import "./tablasEstilos.css"
+function tablaDañoN() {
+  return (
+    <table>
+        <thead> 
+            <tr>
+                <th>Tipo(Especie)</th>
+                <th>Raza</th>
+                <th>N° Animales Afectados</th>
+                <th>N° Animales Muertos</th>
+                <th>Necesidades Inmediatas</th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+          </tr>
+          <tr>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+          </tr>
+          <tr>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+          </tr>
+          <tr>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+          </tr>
+          <tr>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+          </tr> 
+          <tr>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+            <td><input type="text" name="" id="" /></td>
+          </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td>Total</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tfoot>
+    </table>
+  )
+}
+
+export default tablaDañoN
--- src/components/componentes/compImpactoA.jsx
+++ src/components/componentes/compImpactoA.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import DañoA from '../atomos/tablaDañoA'
 import "./compImpactoA.css"
+import DañoN from '../atomos/tablaDañoN'
 function compImpactoA() {
   return (
     <div className='contenedorImpactoA'>
         <h3>9. IMPACTO AGROPECUARIO</h3>
         <h4>9.1 Daños y necesidades en Agricultura</h4>
         <DañoA/>
-        <h4>9.2 Daños y Necesidades Pecuarias</h4>
-        <h4>9.3 Daño en la Infraestructura Productiva.</h4>
+        <div>
+          <h4>9.2 Daños y Necesidades Pecuarias</h4>
+          <DañoN/>
+          <h4>9.3 Daño en la Infraestructura Productiva.</h4>
+        </div>
     </div>
   )
 }
