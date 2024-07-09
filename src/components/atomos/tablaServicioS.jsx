198a5e2a3f8e7cf3f2f56c3cec7e776713fac280
Oscar Gabriel <cameotapia@gmail.com>
2024-07-09 12:26:34 -0400

    tarde0907

--- src/components/atomos/tablaServicioS.jsx
+++ src/components/atomos/tablaServicioS.jsx
@@ -1,13 +1,52 @@
 import React,{useState} from "react";
 import "./tablaServicioS.css";
 function tablaServicioS() {
+  const initialRows=Array.from({length:6},(_,index)=>({id:index+1}))
+  const [isDañoParcialChecked, setDañoParcialCheked]=useState(false)
+  const [isDañoTotalChecked, setDañoTotalCheked]=useState(false)
+  const [is1erChecked,set1erCheked]=useState(false)
+  const [is2doChecked,set2doCheked]=useState(false)
+  const [is3erChecked,set3erCheked]=useState(false)
+  const handle3erChange=(e)=>{
+    set3erCheked(e.target.checked)
+    if(e.target.checked){
+      set2doCheked(false)
+      set1erCheked(false)
+    }
+  }
+  const handle2doChange=(e)=>{
+    set2doCheked(e.target.checked)
+    if(e.target.checked){
+      set1erCheked(false)
+      set3erCheked(false)
+    }
+  }
+  const handle1erChange=(e)=>{
+    set1erCheked(e.target.checked)
+    if(e.target.checked){
+      set2doCheked(false)
+      set3erCheked(false)
+    }
+  }
+  const handleDañoParcialChange=(e)=>{
+    setDañoParcialCheked(e.target.checked);
+    if(e.target.checked){
+      setDañoTotalCheked(false)
+    }
+  }
+  const handleDañoTotalChange=(e)=>{
+    setDañoTotalCheked(e.target.checked);
+    if(e.target.checked){
+      setDañoParcialCheked(false)
+    }
+  }
   const handleKeyPress=(e)=>{
     const charCode=e.charCode;
     if(charCode<48||charCode>57){
         e.preventDefault();
     }
   }
-  const [rows,setRows]=useState([{id:1}])
+  const [rows,setRows]=useState(initialRows)
   const addRow=()=>{
     setRows([...rows,{id:rows.length+1}])
   }
@@ -154,6 +193,8 @@ function tablaServicioS() {
               type="checkbox"
               name=""
               id=""
+              checked={is1erChecked}
+              onChange={handle1erChange}
             />
           </td>
           <td className="tdServiciosS">
@@ -162,6 +203,8 @@ function tablaServicioS() {
               type="checkbox"
               name=""
               id=""
+              checked={is2doChecked}
+              onChange={handle2doChange}
             />
           </td>
           <td className="tdServiciosS">
@@ -170,6 +213,8 @@ function tablaServicioS() {
               type="checkbox"
               name=""
               id=""
+              checked={is3erChecked}
+              onChange={handle3erChange}
             />
           </td>
           <td className="tdServiciosS">
@@ -178,6 +223,8 @@ function tablaServicioS() {
               type="checkbox"
               name=""
               id=""
+              checked={isDañoParcialChecked}
+              onChange={handleDañoParcialChange}
             />
           </td>
           <td className="tdServiciosS">
@@ -186,102 +233,8 @@ function tablaServicioS() {
               type="checkbox"
               name=""
               id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "95%", border: "none" }}
-              type="text"
-              name=""
-              id=""
-              onKeyPress={handleKeyPress}
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "95%", border: "none" }}
-              type="text"
-              name=""
-              id=""
-              onKeyPress={handleKeyPress}
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "95%", border: "none" }}
-              type="text"
-              name=""
-              id=""
-              onKeyPress={handleKeyPress}
-            />
-          </td>
-        </tr>
-        ))}
-        {rows.map((row,index)=>(
-        <tr key={row.id}>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "90%", border: "none" }}
-              type="text"
-              name=""
-              id=""
-              value={row.id}
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "98%", border: "none" }}
-              type="text"
-              name=""
-              id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "97%", border: "none" }}
-              type="text"
-              name=""
-              id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "92%", border: "none" }}
-              type="checkbox"
-              name=""
-              id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "92%", border: "none" }}
-              type="checkbox"
-              name=""
-              id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "92%", border: "none" }}
-              type="checkbox"
-              name=""
-              id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "90%", border: "none" }}
-              type="checkbox"
-              name=""
-              id=""
-            />
-          </td>
-          <td className="tdServiciosS">
-            <input
-              style={{ width: "86%", border: "none" }}
-              type="checkbox"
-              name=""
-              id=""
+              checked={isDañoTotalChecked}
+              onChange={handleDañoTotalChange}
             />
           </td>
           <td className="tdServiciosS">
